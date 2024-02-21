e4e8eeef4cb973886c14c3061acc020bec27a8b3
gwakjinny <yunjin8931@gmail.com>
2024-02-21 23:22:42 +0900

    :tada: Initial commit

---
+++ src/main/java/com/yunjin/bulkperformancetuning/BulkPerformanceTuningApplication.java
@@ -0,0 +1,13 @@
+package com.yunjin.bulkperformancetuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkPerformanceTuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkPerformanceTuningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunjin/bulkperformancetuning/BulkPerformanceTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunjin.bulkperformancetuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkPerformanceTuningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
