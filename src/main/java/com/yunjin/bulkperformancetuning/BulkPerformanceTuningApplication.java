78836d857cbfbe5f43b10a40e587b60c9f669d92
gwakjinny <yunjin8931@gmail.com>
2024-04-14 16:57:48 +0900

    :bug: @EnableJpaAuditing 추가

--- src/main/java/com/yunjin/bulkperformancetuning/BulkPerformanceTuningApplication.java
+++ src/main/java/com/yunjin/bulkperformancetuning/BulkPerformanceTuningApplication.java
@@ -2,8 +2,10 @@ package com.yunjin.bulkperformancetuning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BulkPerformanceTuningApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/yunjin/bulkperformancetuning/BulkPerformanceTuningApplicationTests.java
+++ src/test/java/com/yunjin/bulkperformancetuning/BulkPerformanceTuningApplicationTests.java
@@ -2,8 +2,10 @@ package com.yunjin.bulkperformancetuning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class BulkPerformanceTuningApplicationTests {
 
 	@Test
